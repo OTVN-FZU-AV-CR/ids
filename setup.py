bd6db5f3d2c5f3b27d0a017cd5c17fc862e9057f
galaunay <gaby.launay@tutanota.com>
2018-01-30 12:34:48 +0000

    Use setuptools

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 # Copyright (c) 2012, 2013, North Carolina State University Aerial Robotics Club
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #     * Redistributions of source code must retain the above copyright
@@ -13,7 +13,7 @@
 #     * Neither the name of the North Carolina State University Aerial Robotics Club
 #       nor the names of its contributors may be used to endorse or promote products
 #       derived from this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -25,7 +25,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 import sys
 
 if sys.platform == "win32":
